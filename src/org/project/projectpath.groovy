8c1464d00294b361656daf0a07861ed33a421c1e
Saad Rahim <saad.rahim@amd.com>
2018-12-05 10:47:34 -0800

    Moving the class

---
+++ src/org/project/projectpath.groovy
@@ -0,0 +1,14 @@
+/* ************************************************************************
+ * Copyright 2018 Advanced Micro Devices, Inc.
+ * ************************************************************************ */
+
+// Paths variables bundled together to reduce parameter bloat on function calls
+class project_paths implements Serializable
+{
+    String project_name
+    String src_prefix
+    String project_src_prefix
+    String build_prefix
+    String project_build_prefix
+    String build_command
+}
