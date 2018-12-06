808a233dcf7dc9ab319675ece0bf3aef55ab84e8
Saad Rahim <saad.rahim@amd.com>
2018-12-06 15:30:15 -0800

    class

--- src/org/amd/docker/docker_image.groovy
+++ src/org/amd/docker/docker_image.groovy
@@ -19,22 +19,20 @@ class docker_image implements Serializable
 	this.stage = stage
 	build_image_name = "build-rocblas-hip-artifactory"
     }
-/*	
-	script {
-	dir( paths.project_src_prefix )
+
+    init()
+    {
+	stage.dir( paths.project_src_prefix )
 	{
-	    def user_uid = sh( script: 'id -u', returnStdout: true ).trim()
+	    def user_uid = stage.sh( script: 'id -u', returnStdout: true ).trim()
 
 	    // Docker 17.05 introduced the ability to use ARG values in FROM statements
 	    // Docker inspect failing on FROM statements with ARG https://issues.jenkins-ci.org/browse/JENKINS-44836
 	    // build_image = docker.build( "${paths.project_name}/${build_image_name}:latest", "--pull -f docker/${build_docker_file} --build-arg user_uid=${user_uid} --build-arg base_image=${from_image} ." )
 
 	    // JENKINS-44836 workaround by using a bash script instead of docker.build()
-	    sh "docker build -t ${paths.project_name}/${build_image_name}:latest -f docker/${docker_args.build_docker_file} ${docker_args.docker_build_args} --build-arg user_uid=${user_uid} --build-arg base_image=${docker_args.from_image} ."
+	    stage.sh "docker build -t ${paths.project_name}/${build_image_name}:latest -f docker/${docker_args.build_docker_file} ${docker_args.docker_build_args} --build-arg user_uid=${user_uid} --build-arg base_image=${docker_args.from_image} ."
 	    image = docker.image( "${paths.project_name}/${build_image_name}:latest" )
-	    }
-
 	}
-*/
-    
+    }
 }
