7b25512a814d51e93344c05b350006b2cd38dae3
Saad Rahim <saad.rahim@amd.com>
2018-12-19 14:20:08 -0800

    Getting things to work

--- vars/buildProject.groovy
+++ vars/buildProject.groovy
@@ -9,33 +9,6 @@ def call(String nodeLogic, project_paths paths, rocDocker docker1, compiler_data
 {   
     node ( nodeLogic )
     {
-        stage ("Checkout source code")
-        {
-            echo "Saad"
-            //build.checkout(paths)
-        }
-        
-        /*stage ("Build Docker Container")
-        {
-            // Build a docker image that represents the library build environment
-            //docker1.buildImage(this)
-        }
-    */
-        /*stage ("Compile Library")
-        {
-            docker.image.inside(docker.runArgs)
-            {
-                withEnv(["CXX=${compiler_args.compiler_path}", 'CLICOLOR_FORCE=1'])
-                {
-                  // Build library & clients
-                  sh  """#!/usr/bin/env bash
-                      set -x
-                      cd ${paths.project_build_prefix}
-                      LD_LIBRARY_PATH=/opt/rocm/hcc/lib ${paths.build_command}
-                    """
-                }                
-            }
-        }*/
         
         body()
     }
