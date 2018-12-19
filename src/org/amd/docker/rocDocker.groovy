b52127de444c04b05b2d359feae9fa071effdb7c
Saad Rahim <saad.rahim@amd.com>
2018-12-19 13:11:08 -0800

    Getting things to work

--- src/org/amd/docker/rocDocker.groovy
+++ src/org/amd/docker/rocDocker.groovy
@@ -20,20 +20,20 @@ class rocDocker implements Serializable
     
     void buildImage(def stage)
     {
-	stage.dir( paths.project_src_prefix )
-	{
-            def user_uid = stage.sh(script: 'id -u', returnStdout: true ).trim()
-            
-            // Docker 17.05 introduced the ability to use ARG values in FROM statements
-            // Docker inspect failing on FROM statements with ARG https://issues.jenkins-ci.org/browse/JENKINS-44836
-            // build_image = docker.build( "${paths.project_name}/${build_image_name}:latest", "--pull -f docker/${buildDockerfile} --build-arg user_uid=${user_uid} --build-arg base_image=${from_image} ." )
-            
-            // JENKINS-44836 workaround by using a bash script instead of docker.build()
-            stage.sh "docker build -t ${paths.project_name}/${buildImageName}:latest -f docker/${buildDockerfile} ${buildArgs} --build-arg user_uid=${user_uid} --build-arg base_image=${baseImage} ."
-            image = stage.docker.image( "${paths.project_name}/${buildImageName}:latest" )
-            // Print system information for the log
-            image.inside( runArgs, insideClosure )
-	}
+        stage.dir( paths.project_src_prefix )
+        {
+                def user_uid = stage.sh(script: 'id -u', returnStdout: true ).trim()
+                
+                // Docker 17.05 introduced the ability to use ARG values in FROM statements
+                // Docker inspect failing on FROM statements with ARG https://issues.jenkins-ci.org/browse/JENKINS-44836
+                // build_image = docker.build( "${paths.project_name}/${build_image_name}:latest", "--pull -f docker/${buildDockerfile} --build-arg user_uid=${user_uid} --build-arg base_image=${from_image} ." )
+                
+                // JENKINS-44836 workaround by using a bash script instead of docker.build()
+                stage.sh "docker build -t ${paths.project_name}/${buildImageName}:latest -f docker/${buildDockerfile} ${buildArgs} --build-arg user_uid=${user_uid} --build-arg base_image=${baseImage} ."
+                image = stage.docker.image( "${paths.project_name}/${buildImageName}:latest" )
+                // Print system information for the log
+                image.inside( runArgs, insideClosure )
+        }
     }
     
 /*    
