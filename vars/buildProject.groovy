1ab58800a50eb8a70323a9d20a3ba949e7966c49
Saad Rahim <saad.rahim@amd.com>
2018-12-19 15:19:01 -0800

    Adding compiler stage

--- vars/buildProject.groovy
+++ vars/buildProject.groovy
@@ -20,7 +20,24 @@ def call(String nodeLogic, project_paths paths, rocDocker docker, compiler_data 
         {
             // Build a docker image that represents the library build environment
             docker.buildImage(this)
-        }           
+        }
+
+        stage ("Compile Library")
+        {
+            docker.image.inside(docker.runArgs)
+            {
+                withEnv(["CXX=${hcc_compiler_args.compiler_path}", 'CLICOLOR_FORCE=1'])
+                {
+                  // Build library & clients
+                  sh  """#!/usr/bin/env bash
+                      set -x
+                      cd ${paths.build_prefix}
+                      LD_LIBRARY_PATH=/opt/rocm/hcc/lib ${paths.build_command}
+                    """
+                }                
+            }
+        }
+        
         body()
     }
 
