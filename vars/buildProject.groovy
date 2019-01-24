be917be4eaa57ed2adec7d7ebd3affe4d0471154
Saad Rahim <saad.rahim@amd.com>
2019-01-24 15:40:09 -0800

    Fixing compile

--- vars/buildProject.groovy
+++ vars/buildProject.groovy
@@ -5,7 +5,7 @@
 import com.amd.project.*
 import com.amd.docker.rocDocker
 
-def call(String nodeLogic, boolean runFormatCheck, boolean buildPackage, project_paths paths, rocDocker docker, compiler_data hcc_compiler_args, rocTest libTest, Closure body)
+def call(String nodeLogic, boolean runFormatCheck, boolean buildPackage, project_paths paths, rocDocker docker, compiler_data hcc_compiler_args, rocTests libTest, Closure body)
 {   
     node ( nodeLogic )
     {
