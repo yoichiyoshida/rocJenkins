0a54a474cf4a729a1c21c4094fadd57e18f556fd
Saad Rahim <saad.rahim@amd.com>
2019-02-04 15:10:37 -0800

    refactoring

--- src/com/amd/docker/rocDocker.groovy
+++ src/com/amd/docker/rocDocker.groovy
@@ -1,7 +1,7 @@
 /* ************************************************************************
  * Copyright 2018-2019 Advanced Micro Devices, Inc.
  * ************************************************************************ */
-package com.amd.rocDocker
+package com.amd.docker.rocDocker
 import java.nio.file.Path;
 
 // Docker related variables gathered together to reduce parameter bloat on function calls
