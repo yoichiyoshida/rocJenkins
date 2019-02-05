248b87842db0e33e9783dd49398ffa85e6d86592
Saad Rahim <saad.rahim@amd.com>
2019-02-05 10:58:27 -0800

    Silly error

--- src/com/amd/project/rocProject.groovy
+++ src/com/amd/project/rocProject.groovy
@@ -6,7 +6,6 @@ package com.amd.project
 
 import com.amd.project.*
 
-// Paths variables bundled together to reduce parameter bloat on function calls
 class rocProject implements Serializable
 {
     project_paths paths
